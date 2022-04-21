63ce772c89f900d0d4d0a1687980a9484c13681f
Damara Syaidil <damaraccd11@gmail.com>
2022-04-21 10:22:17 +0700

    Add: lecture week 4

---
+++ golang-beginner/4-pointer-struct-slice/struct/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// membuat struct Rectangle dengan dua atribut yaitu Width dan Length
+
+type Rectangle struct {
+	Width  int
+	Length int
+}
+
+func main() {
+	var r Rectangle
+	r.Width = 10
+	r.Length = 20
+	fmt.Println(r) //{10,20}
+
+	r2 := Rectangle{Width: 5, Length: 15}
+	fmt.Println(r2) // {5,15}
+
+	fmt.Println("lebar rectangle2:", r2.Width)    //5
+	fmt.Println("panjang rectangle2:", r2.Length) //15
+
+}
