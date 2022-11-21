39d2d2c01caff704631041bd17449428a0c03695
Damara Syaidil <damaraccd11@gmail.com>
2022-11-21 22:13:16 +0700

    add: materi fundamental

--- 1-golang-fundamental/4-array/4-len-array-act/main.go
+++ 1-golang-fundamental/4-array/4-len-array-act/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 // Nah sekarang kalian coba outputkan nilai terakhir dari array/slice dibawah
-// manfaatkan method len() ya.
+// manfaatkan syntax len() ya.
+
+// Output: 
+// Panjang array adalah 69
+// Nilai terakhir dari array adalah 51
+
 func main() {
-	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+	array := [69]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 		11, 14, 15, 17, 19, 20, 21, 24, 26, 29,
 		71, 84, 85, 87, 19, 20, 61, 24, 56, 52,
 		71, 84, 85, 87, 19, 20, 61, 24, 56, 53,
@@ -13,7 +18,6 @@ func main() {
 		71, 84, 85, 87, 19, 20, 61, 24, 56, 51,
 		7, 15, 85, 19, 20, 61, 24, 56, 51,
 	}
-	
-	fmt.Println(array[len(array)-1])
-	
+	// TODO: answer here
+
 }
