fb6dc2d04c9ac05a017e0e98f82dccacfbcc833c
Damara Syaidil <damaraccd11@gmail.com>
2022-11-29 16:41:51 +0700

    add: lecture fund web

--- 1-golang-fundamental/5-slice/2-read-slice-act/main.go
+++ 1-golang-fundamental/5-slice/2-read-slice-act/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // Disini kalian coba untuk membaca nilai dari slice yang diberikan.
-// Lalu kalian akan menambahkan kata "Olleh" pada slice tersebut.
+// Lalu kalian akan menambahkan kata "nama kalian" pada slice tersebut.
 func main() {
 	slice := []string{"Hello", "World"}
 	// TODO: answer here
